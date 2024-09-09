31db820ad025567acc49bc8d9a9021b45bb7f0b7
Evandro Martinelli <evandro@evandro>
2024-09-09 17:06:44 -0300

    logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','G-2QPMPZ3WQ3');`,
+      })(window,document,'script','dataLayer','G-D9M36QETC8');`,
             }}
           />
         </Head>
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <NextScript />
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-2QPMPZ3WQ3" height="0" width="0" style="display: none; visibility: hidden;" />`,
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-D9M36QETC8" height="0" width="0" style="display: none; visibility: hidden;" />`,
             }}
           />
         </body>
